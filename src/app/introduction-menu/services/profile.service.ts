25828e3121e0ef27719830256a9e0759f7cdf2aa
mkovetsky <mkovetsky@gmail.com>
2018-06-09 15:24:57 +0300

    header card details navigation

--- src/app/introduction-menu/services/profile.service.ts
+++ src/app/introduction-menu/services/profile.service.ts
@@ -20,24 +20,27 @@ export class ProfileService {
 
   profileSections: ProfileCategory[] = [
     {
-      categoryTitle: 'Academics',
+      categoryTitle: 'Trips',
       icon: 'school',
       records: [
         {img: '../../../assets/introduction-menu/newfound.png', title: 'Geographic Information System', description: 'Undergraduate'},
         {img: '../../../assets/introduction-menu/concordia.png', title: 'Industrial Design', description: 'Undergraduate'}
       ],
+      link: '/card-details/trip-details'
     },
     {
-      categoryTitle: 'Work Experiences',
+      categoryTitle: 'Activities',
       icon: 'work',
+      link: '/card-details/activity-details',
       records: [
         {img: '../../../assets/introduction-menu/autodesk.png', title: 'Junior Business Analyst', description: 'Since 2010'},
         {img: '../../../assets/introduction-menu/lixar.png', title: 'Junior Business Analyst', description: 'Since 2010'}
       ],
     },
     {
-      categoryTitle: 'Skill Sets',
+      categoryTitle: 'Academic',
       icon: 'home',
+      link: '/card-details/work-experience',
       records: [
       ],
     },
@@ -48,10 +51,12 @@ export class ProfileService {
         {img: '../../../assets/introduction-menu/soccer.png', title: 'Soccer', description: 'Since 2010'},
         {img: '../../../assets/introduction-menu/game.png', title: 'Video Game', description: 'Since 2010'}
       ],
+      link: '/card-details/school-details-highlights'
     },
     {
-      categoryTitle: 'Travels',
+      categoryTitle: 'Travelling',
       icon: 'flight',
+      link: '/card-details/travelling-info',
       records: [
         {img: '../../../assets/introduction-menu/cambodia.png', title: 'Cambodia', description: 'July 2010'},
         {img: '../../../assets/introduction-menu/usa.png', title: 'Usa', description: 'March 2010'}
